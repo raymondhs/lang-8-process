21a45c9a8fa968181d95073ea13affec03b99cc4
Raymond Hendy <raymhs91@gmail.com>
2018-04-02 15:10:27 +0800

    Keep identical pairs in separate files

--- clean_corpus.sh
+++ clean_corpus.sh
@@ -14,12 +14,15 @@ if [ ! -f clean-corpus-n.perl ]; then
   wget https://github.com/moses-smt/mosesdecoder/raw/master/scripts/training/clean-corpus-n.perl
 fi
 
-python preprocess_lang8.py lang-8-20111007-L1-v2.dat lang-8-v2.src lang-8-v2.tgt
-for suf in src tgt; do
-  cat lang-8-v2.$suf \
-    | perl replace-unicode-punctuation.perl \
-    | perl remove-non-printing-char.perl \
-    | perl normalize-punctuation.perl \
-    | python nltk_tokenize.py > lang-8-v2.tok.$suf
+python preprocess_lang8.py lang-8-20111007-L1-v2.dat lang-8-v2 lang-8-v2.keep src tgt
+for prefix in lang-8-v2 lang-8-v2.keep; do
+  for suf in src tgt; do
+    cat $prefix.$suf \
+      | perl replace-unicode-punctuation.perl \
+      | perl remove-non-printing-char.perl \
+      | perl normalize-punctuation.perl \
+      | python nltk_tokenize.py > $prefix.tok.$suf
+  done
 done
 perl clean-corpus-n.perl lang-8-v2.tok src tgt lang-8-v2.clean 1 80
+perl clean-corpus-n.perl lang-8-v2.keep.tok src tgt lang-8-v2.keep.clean 1 80
--- preprocess_lang8.py
+++ preprocess_lang8.py
@@ -12,6 +12,7 @@ import multiprocessing
 import langid
 
 NUM_JOBS = 8
+AGGRESIVE = False
 
 SLINE_PATTERN = re.compile(r'\[sline\].*?\[/sline\]')
 
@@ -26,6 +27,7 @@ def remove_tags(line):
 
 def process(line):
     sentence_pairs = []
+    unchanged_sentence_pairs = []
     row = json.loads(re.sub(r'[\x00-\x1F]+', '', line))
     if row[2] == 'English':
         for i in range(len(row[4])):
@@ -33,35 +35,56 @@ def process(line):
             src_lang, _ = langid.classify(src_sent)
             if src_lang != 'en':
                 continue
+            src_sent = re.sub('\s+', ' ', src_sent)
+            has_added = False
+            if len(row[5][i]) == 0:
+                unchanged_sentence_pairs.append((src_sent, src_sent))
+                has_added = True
             for tgt_sent in row[5][i]:
-                if not tgt_sent or tgt_sent.strip() == src_sent:
+                if not tgt_sent:
+                    continue
+                tgt_sent = re.sub('\s+', ' ', tgt_sent)
+                if tgt_sent.strip() == src_sent:
+                    if not has_added:
+                        unchanged_sentence_pairs.append((src_sent, src_sent))
+                        has_added = True
                     continue
                 tgt_lang, _ = langid.classify(tgt_sent)
                 if tgt_lang != 'en':
                     continue
-                src_sent = re.sub('\s+', ' ', src_sent)
                 tgt_sent = remove_tags(tgt_sent)
                 tgt_sent = tgt_sent.strip()
-                if tgt_sent \
-                   and tgt_sent[0] == tgt_sent[0].upper() \
-                   and tgt_sent[-1] in SENT_END:
+                if tgt_sent and \
+                   (not AGGRESIVE or \
+                    (tgt_sent[0] == tgt_sent[0].upper() \
+                    and tgt_sent[-1] in SENT_END)):
                     sentence_pairs.append((src_sent, tgt_sent))
-    return sentence_pairs
+    return sentence_pairs, unchanged_sentence_pairs
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print "usage: <raw input> <output.src> <output.tgt>"
+    if len(sys.argv) != 6:
+        print "usage: <raw input> <output> <keep> src tgt"
         sys.exit(1)
 
     print "Start processing"
     start_time = time.time()
     pool = multiprocessing.Pool(NUM_JOBS)
     results = [pool.apply_async(process, (line,)) for line in open(sys.argv[1])]
-    with open(sys.argv[2], 'w') as src_f, \
-         open(sys.argv[3], 'w') as tgt_f:
-        for sentence_pairs in results:
-            for src_sent, tgt_sent in sentence_pairs.get():
+    src_fn = sys.argv[2]+'.'+sys.argv[4]
+    tgt_fn = sys.argv[2]+'.'+sys.argv[5]
+    src_keep_fn = sys.argv[3]+'.'+sys.argv[4]
+    tgt_keep_fn = sys.argv[3]+'.'+sys.argv[5]
+    with open(src_fn, 'w') as src_f, \
+         open(tgt_fn, 'w') as tgt_f, \
+         open(src_keep_fn, 'w') as src_keep_f, \
+         open(tgt_keep_fn, 'w') as tgt_keep_f:
+        for result in results:
+            sentence_pairs, unchanged_sentence_pairs = result.get()
+            for src_sent, tgt_sent in sentence_pairs:
                 src_f.write(src_sent.encode('utf-8')+'\n')
                 tgt_f.write(tgt_sent.encode('utf-8')+'\n')
+            for src_sent, tgt_sent in unchanged_sentence_pairs:
+                src_keep_f.write(src_sent.encode('utf-8')+'\n')
+                tgt_keep_f.write(tgt_sent.encode('utf-8')+'\n')
     print "Done in", time.time()-start_time, "seconds"
     
